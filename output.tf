93d89952d0b85f97daf35685cddad267f14b9e66
Luke Davitt <ldavitt@morpheusdata.com>
2024-04-04 08:28:57 -0400

    push

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
-output "vm_ip" {
-  value = data.vsphere_virtual_machine.vm.guest_ip_addresses[0]
+output "example_output1" {
+  value = "This is testes 1"
+}
+
+output "example_output2" {
+  value = "This is testes 2"
+}
+
+output "example_output3" {
+  value = "This is testes 3.  Its funny because im saying testes instead of test."
 }
