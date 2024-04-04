c93b9d5b11f3358af0834af11100cc4462111575
Luke Davitt <ldavitt@morpheusdata.com>
2024-04-04 08:23:07 -0400

    stuff

--- variables.tf
+++ variables.tf
@@ -1,3 +1,28 @@
 variable "instanceName" {
   default = "qa-test-terraform"
 }
+
+variable "cloudUrl" {
+  type        = string
+  description = "cloud url"
+  default     = ""
+}
+
+variable "cloudUsername" {
+  type        = string
+  description = "cloudUsername"
+  default     = ""
+}
+
+variable "cloudPassword" {
+  type        = string
+  description = "cloudPassword"
+  default     = ""
+}
+
+variable "datacenterName" {
+  type        = string
+  description = "datacenterName"
+  default     = ""
+  
+}
