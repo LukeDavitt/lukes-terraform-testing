3e708fc4740b7ad37831afd5711b6c6bee2a0a2a
Luke Davitt <ldavitt@morpheusdata.com>
2024-04-05 07:24:58 -0400

    update

---
+++ outyputty.tf
@@ -0,0 +1,11 @@
+output "example_output4" {
+  value = "This is testes 1"
+}
+
+output "example_output5" {
+  value = "This is testes 2"
+}
+
+output "example_output6" {
+  value = "This is testes 3.  Its funny because im saying testes instead of test."
+}
